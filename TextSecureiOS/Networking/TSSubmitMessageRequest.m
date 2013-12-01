a06532ba9f2e27e72ef914224c0020507d7795cb
Christine Corbett Moran <christine.corbett@gmail.com>
2013-12-01 11:37:37 +0100

    messing around trying to debug

--- TextSecureiOS/Networking/TSSubmitMessageRequest.m
+++ TextSecureiOS/Networking/TSSubmitMessageRequest.m
@@ -23,16 +23,26 @@
                                                              nil]
                                             forKeys:[[NSArray alloc] initWithObjects:@"type",@"destination",@"body",@"timestamp" ,nil]];
 
-  if ([contact.relay length]>0) {
-    [messageDictionary setObject:contact.relay forKey:@"relay"];
-  }
-  else {
-#warning should change this to whatever default should be or leave empty as Moxie specifies
-    [messageDictionary setObject:textSecureServer forKey:@"relay"];
+  // Moxies messages
+  messageDictionary = [[NSMutableDictionary alloc]
+                                            initWithObjects:[[NSArray alloc]
+                                                             initWithObjects:[NSNumber numberWithInt:3],
+                                                             @"+41795994505",
+                                                             @"Igijh9QEEiEFnOoLwvn2gM6F1KFB9mb6VereQj2yza4FKKgc2cGn820aIQW4uJUw/HJTj4CZf/7JJBeL9ZPSUzxIqxDSm+KmVFwFeiJFIgohBd6xcuFRwYyqCA7PwPvtwq52dz+VqRSTdfQtYdpcGoAZEAAYACITVgrobdugC+NH+vNGYfMdR225dq2a+KETkpoq",
+                                                             nil]
+                                            forKeys:[[NSArray alloc] initWithObjects:@"type",@"destination",@"body",nil]];
+//  if ([contact.relay length]>0) {
+//    [messageDictionary setObject:contact.relay forKey:@"relay"];
+//  }
+//  else {
+//#warning should change this to whatever default should be or leave empty as Moxie specifies
+//    [messageDictionary setObject:textSecureServer forKey:@"relay"];
+//    
+//  }
 
-  }
   self = [super initWithURL:[NSURL URLWithString:textSecureMessagesAPI]];
   NSMutableDictionary *allMessages = [[NSMutableDictionary alloc] initWithObjectsAndKeys:[[NSArray alloc] initWithObjects: messageDictionary,nil],@"messages", nil];
+  NSLog(@"sending to server %@",allMessages);
   [self setHTTPMethod:@"POST"];
   [self setParameters:allMessages];
   return self;
